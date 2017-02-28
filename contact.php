71b0ce41ba7304b935b0c1f86020e2f69beea9c9
unknown <aassou.abdelilah@gmail.com>
2017-02-28 14:33:08 +0000

    Fixing Issues - Date : 28/02/2017

--- contact.php
+++ contact.php
@@ -29,21 +29,6 @@
     <!-- Document Title
     ============================================= -->
     <title>Groupe Annahda | Contact</title>
-    <!-- Piwik -->
-    <script type="text/javascript">
-      var _paq = _paq || [];
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-        var u="//costetics.esy.es/admin/piwik/";
-        _paq.push(['setTrackerUrl', u+'piwik.php']);
-        _paq.push(['setSiteId', 1]);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-      })();
-    </script>
-    <noscript><p><img src="//costetics.esy.es/admin/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
-    <!-- End Piwik Code -->
 </head>
 
 <body class="stretched">
