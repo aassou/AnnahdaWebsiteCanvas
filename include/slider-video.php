c0bdaca8503186b258e2f125c601b06889087459
unknown <aassou.abdelilah@gmail.com>
2017-03-02 23:37:27 +0000

    URL rewriting - Date : 02/03/2017

--- include/slider-video.php
+++ include/slider-video.php
@@ -1,6 +1,6 @@
 <section class="slider-parallax swiper_wrapper full-screen clearfix">
 <video style="width: 100%; height:80%;" controls="controls" autoplay="autoplay">
-  <source src="videos/annahda-video-slider.mp4">
+  <source src="/videos/annahda-video-slider.mp4">
   Votre navigateur ne gère pas l'élément <code>video</code>.
 </video>
 <!--iframe type="text/html" width="100%" height="80%" src="https://www.youtube.com/embed/ec5itOYy15Q?html5=1&autoplay=1&rel=0&showinfo=0&controls=0&modestbranding=0" frameborder="0"></iframe-->
