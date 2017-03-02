1c07215ef81c6eedc9e16fcec4a458b53b5508f9
unknown <aassou.abdelilah@gmail.com>
2017-03-02 23:06:31 +0000

    URL rewriting - Date : 02/03/2017

--- de/index.php
+++ de/index.php
@@ -26,10 +26,8 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en-US">
 <head>
-
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="author" content="SemiColonWeb" />
-
     <!-- Stylesheets
     ============================================= -->
     <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
@@ -39,29 +37,23 @@
     <link rel="stylesheet" href="../css/font-icons.css" type="text/css" />
     <link rel="stylesheet" href="../css/animate.css" type="text/css" />
     <link rel="stylesheet" href="../css/magnific-popup.css" type="text/css" />
-
     <link rel="stylesheet" href="../css/responsive.css" type="text/css" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <!--[if lt IE 9]>
         <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
     <![endif]-->
-
     <!-- External JavaScripts
     ============================================= -->
     <script type="text/javascript" src="../js/jquery.js"></script>
     <script type="text/javascript" src="../js/plugins.js"></script>
-
     <!-- Document Title
     ============================================= -->
     <title>Gruppe Annahda</title>
 </head>
-
 <body class="stretched">
-
     <!-- Document Wrapper
     ============================================= -->
     <div id="wrapper" class="clearfix">
-
         <!-- Header
         ============================================= -->
         <?php
@@ -71,10 +63,10 @@
         <!-- #slider begin -->
         <?php 
         if ( $config->sliderType() == 1 ) {
-            include("/include/slider-video.php");
+            include("/../include/slider-video.php");
         }
         else {
-            include("/include/slider-images.php");
+            include("/../include/slider-images.php");
         }     
         ?>
         <!-- #slider end -->
@@ -91,20 +83,16 @@
         }
         ?>
         <!-- #content end -->
-
         <!-- Footer
         ============================================= -->
         <?php
         include('include/footer.php');
         ?>
         <!-- #footer end -->
-            
     </div><!-- #wrapper end -->
-
     <!-- Go To Top
     ============================================= -->
     <div id="gotoTop" class="icon-angle-up"></div>
-
     <!-- Footer Scripts
     ============================================= -->
     <script type="text/javascript" src="../js/functions.js"></script>
