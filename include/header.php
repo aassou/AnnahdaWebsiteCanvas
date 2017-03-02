4ce48303102726ec5c84d6231f502723334364d2
unknown <aassou.abdelilah@gmail.com>
2017-03-02 22:20:44 +0000

    URL rewriting - Date : 02/03/2017

--- include/header.php
+++ include/header.php
@@ -16,7 +16,7 @@
             ============================================= -->
             <nav id="primary-menu" class="dark">
                 <ul>
-                    <li <?= ($currentPage == "home") ? 'class="current"' : '' ?> >
+                    <li <?= ($currentPage == "index.php") ? 'class="current"' : '' ?> >
                         <a href="/home" title="Français"><img src="/images/icons/flags/french.png" /></a>
                     </li>
                     <li <?= ($currentPage == "index.php") ? 'class="current"' : '' ?> >
@@ -28,17 +28,17 @@
                     <li <?= ($currentPage == "index.php") ? 'class="current"' : '' ?> >
                         <a href="/es/index.php" title="Español"><img src="/images/icons/flags/spain.png" /></a>
                     </li>
-                    <li <?= ($currentPage == "home") ? 'class="current"' : '' ?> >
+                    <li <?= ($currentPage == "index.php") ? 'class="current"' : '' ?> >
                         <a href="/home"><div>Accueil</div></a>
                     </li>
-                    <li <?= ($currentPage == "projects" || $currentPage == "project") ? 'class="current"' : '' ?> >
+                    <li <?= ($currentPage == "portfolio.php" || $currentPage == "portfolio-single-gallery.php") ? 'class="current"' : '' ?> >
                         <a href="/projects"><div>Nos Projets</div></a>
                     </li>
                     <!--li ><a href="#"><div>Notre Offre</div></a></li-->
-                    <li <?= ($currentPage == "about") ? 'class="current"' : '' ?>>
+                    <li <?= ($currentPage == "about.php") ? 'class="current"' : '' ?>>
                         <a href="/about"><div>A propos du groupe</div></a>
                     </li>
-                    <li <?= ($currentPage == "contact") ? 'class="current"' : '' ?>>
+                    <li <?= ($currentPage == "contact.php") ? 'class="current"' : '' ?>>
                         <a href="/contact"><div>Contact</div></a>
                     </li>
                 </ul>
--- portfolio.php
+++ portfolio.php
@@ -151,15 +151,15 @@
                         ?>
                         <article class="portfolio-item pf-media pf-icons">
                             <div class="portfolio-image">
-                                <a href="portfolio-single-gallery.php?idProjet=<?= $projet->id() ?>">
+                                <a href="/project/<?= $projet->id() ?>">
                                     <?= $image ?>
                                 </a>
                                 <div class="portfolio-overlay">
-                                    <a href="portfolio-single-gallery.php?idProjet=<?= $projet->id() ?>" class="left-icon"><i class="icon-line-plus"></i></a>
+                                    <a href="/project/<?= $projet->id() ?>" class="left-icon"><i class="icon-line-plus"></i></a>
                                 </div>
                             </div>
                             <div class="portfolio-desc">
-                                <h3><a href="portfolio-single-gallery.php?idProjet=<?= $projet->id() ?>"><?= $projet->name() ?></a></h3>
+                                <h3><a href="/project/<?= $projet->id() ?>"><?= $projet->name() ?></a></h3>
                                 <!--span><a href="#">Media</a>, <a href="#">Icons</a></span-->
                             </div>
                         </article>
