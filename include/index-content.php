5e2e6ee19b4b1341417fea901910a728649d6f0f
unknown <aassou.abdelilah@gmail.com>
2017-03-07 23:18:12 +0000

    Fix issues - Date : 07/03/2017

--- include/index-content.php
+++ include/index-content.php
@@ -124,7 +124,7 @@
                 $image = "";
                 if ( $imageManager->getImageNumberByIdProjet($projet->id()) >= 1 ) {
                     $image = $imageManager->getFirstImageByIdProjet($projet->id());
-                    $image = '<img src="'.$image->url().'-/resize/176x160/" />';    
+                    $image = '<img src="'.$image->url().'-/resize/376x360/" />';    
                 }
                 else {
                     $image = '<img src="images/logo_white.jpg" />'; 
