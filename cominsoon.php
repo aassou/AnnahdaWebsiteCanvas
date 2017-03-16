6155f5df5a9d0eaf313798ed5b7b26267b65847d
unknown <aassou.abdelilah@gmail.com>
2017-03-16 15:05:45 +0000

    Fix issues - Date : 16/03/2017

---
+++ cominsoon.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html class="no-js">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Groupe Annahda Lil Iaamar</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap and Font Awesome css-->
+    <link rel="stylesheet" href="http://soon.ondrejsvestka.cz/css/font-awesome.css">
+    <link rel="stylesheet" href="http://soon.ondrejsvestka.cz/css/bootstrap.min.css">
+    <!-- Google fonts-->
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Pacifico">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <!-- Theme stylesheet-->
+    <link rel="stylesheet" href="http://soon.ondrejsvestka.cz/css/style.default.css" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="http://soon.ondrejsvestka.cz/css/custom.css">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="http://soon.ondrejsvestka.cz/favicon.png">
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  </head>
+  <body>
+    <div style="background-image: url('http://soon.ondrejsvestka.cz/img/paris.jpg')" class="main"> 
+      <div class="overlay"></div>
+      <div class="container">
+        <p class="social"><a target="_blank" href="https://www.facebook.com/groupeannahdaliliaamar/?fref=ts" title="" class="facebook"><i class="fa fa-facebook"></i></a><a target="_blank" href="https://www.youtube.com/channel/UCp3-xSrmFlb6tZ7vjimUbVw" title="" class="gplus"><i class="fa fa-youtube"></i></a></p>
+        <h1 class="cursive">Groupe Annahda Lil Iaamar</h1>
+        <h2 class="sub">Le nouveau site du Groupe Annahda Lil Iaamar est en cours de construction, Merci d'être patient. </h2>
+      </div>
+      <div class="footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <p>&copy;2017 Groupe Annahda Lil Iaamar</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- JAVASCRIPT FILES -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="javascripts/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+    <script src="http://soon.ondrejsvestka.cz/js/bootstrap.min.js"></script>
+    <script src="http://soon.ondrejsvestka.cz/js/jquery.cookie.js"></script>
+    <script src="http://soon.ondrejsvestka.cz/js/front.js"></script>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,52 +1,68 @@
-
+<?php
+//classes loading begin
+    function classLoad ($myClass) {
+        if(file_exists('admin/model/'.$myClass.'.php')){
+            include('admin/model/'.$myClass.'.php');
+        }
+        elseif(file_exists('admin/controller/'.$myClass.'.php')){
+            include('admin/controller/'.$myClass.'.php');
+        }
+    }
+    spl_autoload_register("classLoad"); 
+    include('include/config.php');
+    //classes loading end
+    //class managers
+    $sliderImageManager = new SliderImageManager($pdo);
+    $sliderVideoManager = new SliderVideoManager($pdo);
+    $configManager = new ConfigManager($pdo);
+    $projetManager = new ProjetManager($pdo);
+    $imageManager = new ImageManager($pdo);
+    //classes
+    $projets = $projetManager->getProjets();
+    $sliderImages = $sliderImageManager->getSliderImages();
+    $sliderVideos = $sliderVideoManager->getSliderVideos();
+    $config = $configManager->getConfig();
+?>
 <!DOCTYPE html>
-<html class="no-js">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<html dir="ltr" lang="en-US">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="Groupe Annahda Lil Iaamar" />
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="/css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="/style.css" type="text/css" />
+    <link rel="stylesheet" href="/css/dark.min.css" type="text/css" />
+    <link rel="stylesheet" href="/css/font-icons.css" type="text/css" />
+    <link rel="stylesheet" href="/css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="/css/magnific-popup.css" type="text/css" />
+    <link rel="stylesheet" href="/css/responsive.css" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <!--[if lt IE 9]>
+        <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <![endif]-->
+    <script type="text/javascript" src="/js/jquery.js"></script>
+    <script type="text/javascript" src="/js/plugins.js"></script>
     <title>Groupe Annahda Lil Iaamar</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="all,follow">
-    <!-- Bootstrap and Font Awesome css-->
-    <link rel="stylesheet" href="http://soon.ondrejsvestka.cz/css/font-awesome.css">
-    <link rel="stylesheet" href="http://soon.ondrejsvestka.cz/css/bootstrap.min.css">
-    <!-- Google fonts-->
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Pacifico">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
-    <!-- Theme stylesheet-->
-    <link rel="stylesheet" href="http://soon.ondrejsvestka.cz/css/style.default.css" id="theme-stylesheet">
-    <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="http://soon.ondrejsvestka.cz/css/custom.css">
-    <!-- Favicon-->
-    <link rel="shortcut icon" href="http://soon.ondrejsvestka.cz/favicon.png">
-    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-  </head>
-  <body>
-    <div style="background-image: url('http://soon.ondrejsvestka.cz/img/paris.jpg')" class="main"> 
-      <div class="overlay"></div>
-      <div class="container">
-        <p class="social"><a target="_blank" href="https://www.facebook.com/groupeannahdaliliaamar/?fref=ts" title="" class="facebook"><i class="fa fa-facebook"></i></a><a target="_blank" href="https://www.youtube.com/channel/UCp3-xSrmFlb6tZ7vjimUbVw" title="" class="gplus"><i class="fa fa-youtube"></i></a></p>
-        <h1 class="cursive">Groupe Annahda Lil Iaamar</h1>
-        <h2 class="sub">Le nouveau site du Groupe Annahda Lil Iaamar est en cours de construction, Merci d'être patient. </h2>
-      </div>
-      <div class="footer">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6">
-              <p>&copy;2017 Groupe Annahda Lil Iaamar</p>
-            </div>
-          </div>
-        </div>
-      </div>
+</head>
+<body class="stretched">
+    <div id="wrapper" class="clearfix">
+        <?php include("include/header.php"); ?>
+        <?php if ( $config->sliderType() == 1 ) { include("include/slider-video.php");}else { include("include/slider-images.php"); }?>
+        <?php
+        //if the value of indexContent is 0, this means that our page index is composed of slider and content
+        if ( $config->indexContent() == 0 ) {
+            include('include/index-content.php');
+        }
+        //if the value of indexContent is 2, this means that our page index is composed of slider and minimal content
+        else if ( $config->indexContent() == 2 ) {
+            include('include/index-content-min.php');
+        }
+        ?>
+        <?php
+        include('include/footer.php');
+        ?>  
     </div>
-    <!-- JAVASCRIPT FILES -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="javascripts/vendor/jquery-1.11.0.min.js"><\/script>')</script>
-    <script src="http://soon.ondrejsvestka.cz/js/bootstrap.min.js"></script>
-    <script src="http://soon.ondrejsvestka.cz/js/jquery.cookie.js"></script>
-    <script src="http://soon.ondrejsvestka.cz/js/front.js"></script>
-  </body>
+    <div id="gotoTop" class="icon-angle-up"></div>
+    <script type="text/javascript" src="/js/functions.js"></script>
+</body>
 </html>
